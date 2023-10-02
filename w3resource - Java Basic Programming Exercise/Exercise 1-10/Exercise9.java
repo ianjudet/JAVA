454de0a82fb9494677c059fbcbfaaf32b49d5b2a
ianjudet <133672069+ianjudet@users.noreply.github.com>
2023-10-02 17:19:51 +0800

    Add files via upload

---
+++ w3resource - Java Basic Programming Exercise/Exercise 1-10/Exercise9.java
@@ -0,0 +1,21 @@
+/*
+ * 9. Write a Java program to compute the specified expressions and print the output.
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output
+2.138888888888889
+ */
+
+
+public class Exercise9 
+{
+    public static void main(String[] args)
+    {
+
+        double num = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+        System.out.println("Expected Output");
+        System.out.print(num);
+    }
+
+    
+}
